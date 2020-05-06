2ae5d71cded37d0a236a70d058fad14bade17cff
kharioki <antolisah@gmail.com>
2020-05-06 14:33:46 +0300

    added styling and basic components

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function AddTransaction() {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <React.Fragment>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            placeholder="Enter text..."
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default AddTransaction;
