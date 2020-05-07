5cb49fd7ca92a4419c220c5e5ba3b3865196cc21
kharioki <antolisah@gmail.com>
2020-05-07 17:28:31 +0300

    added a live app build config

---
+++ client/build/precache-manifest.1aefa9db46f19a38bac49c9f1a94f6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1f16b175f275a12dc0def71320d9e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a5eff90083bf42c849",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "f242d7c4b34c3f40c1bc",
+    "url": "/static/js/2.00e602dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00e602dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a5eff90083bf42c849",
+    "url": "/static/js/main.be2b31db.chunk.js"
+  },
+  {
+    "revision": "27b00dc97d8be027a4ee",
+    "url": "/static/js/runtime-main.745e75de.js"
+  }
+]);
